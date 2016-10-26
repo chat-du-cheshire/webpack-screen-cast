85c21617b17512f48eae8292175b3b920850e20d
Максим <maxlvan@yandex.ru>
2016-10-26 22:49:37 +0300

    Объединение модулей

--- frontend/app.js
+++ frontend/app.js
@@ -6,6 +6,14 @@ window.onload = function(){
         require.ensure([], function(require){
             let login = require('./login');
             login();
-        });
+        }, 'auth'); /* Указывает webpack имя файла в который собираются модули */
+    }
+
+    document.getElementById('logout').onclick = function() {
+        /*Подгружает модуль в [] или в require в callback*/
+        require.ensure([], function(require){
+            let logout = require('./logout');
+            logout();
+        }, 'auth'); /* Указывает webpack имя файла в который собираются модули */
     }
 }
---
+++ public/js/1.auth.js
@@ -0,0 +1,24 @@
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	module.exports = function () {
+	    console.log("Login!");
+	};
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	module.exports = function () {
+	    console.log('Logout!');
+	};
+
+/***/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS5hdXRoLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vL2Zyb250ZW5kL2xvZ2luLmpzIiwid2VicGFjazovLy8/ZDQxZCIsIndlYnBhY2s6Ly8vZnJvbnRlbmQvbG9nb3V0LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gZnVuY3Rpb24oKXtcbiAgICBjb25zb2xlLmxvZyhcIkxvZ2luIVwiKTtcbn1cblxuXG4vKiogV0VCUEFDSyBGT09URVIgKipcbiAqKiBmcm9udGVuZC9sb2dpbi5qc1xuICoqLyIsInVuZGVmaW5lZFxuXG5cbi8qKiBXRUJQQUNLIEZPT1RFUiAqKlxuICoqIFxuICoqLyIsIm1vZHVsZS5leHBvcnRzID0gZnVuY3Rpb24oKXtcbiAgICBjb25zb2xlLmxvZygnTG9nb3V0IScpXG59XG5cblxuLyoqIFdFQlBBQ0sgRk9PVEVSICoqXG4gKiogZnJvbnRlbmQvbG9nb3V0LmpzXG4gKiovIl0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7QUNDQTs7Ozs7Ozs7O0FDREE7QURDQTs7OzsiLCJzb3VyY2VSb290IjoiIn0=
